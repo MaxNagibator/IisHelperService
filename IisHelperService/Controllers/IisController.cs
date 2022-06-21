952ea9abfa0ab14b38c15cdbc47e93a13142e9ba
Грицина Максим <bob217@mail.ru>
2022-06-22 00:18:40 +0700

    скопипастил свою старую приблуду и сделал апи из глины и палок
    - чтото работает, можно развивать
    - хостим в iis и создаём рядом сайтики с помощью сайтика
    - не забываем пулл от админа запускать

---
+++ IisHelperService/Controllers/IisController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Http;
+
+using IisHelperService.Domain;
+
+namespace IisHelperService.Controllers
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> Additional { get; set; }
+    }
+
+    public class CreateSiteRequest
+    {
+        public string PoolName { get; set; }
+        public string SiteName { get; set; }
+        public string SiteUrl { get; set; }
+        public string SiteFolder { get; set; }
+    }
+
+    public class CreateApplicationRequest
+    {
+        public string PoolName { get; set; }
+        public string SiteName { get; set; }
+        public string AppPath { get; set; }
+        public string PhisicalPath { get; set; }
+    }
+
+    public class DeleteSiteWithPullRequest
+    {
+        public string SiteName { get; set; }
+    }
+
+    public class PoolRequest
+    {
+        public string PoolName { get; set; }
+    }
+
+    public class GetPoolWithoutSitesRequest
+    {
+    }
+    public class GetSiteNamesRequest
+    {
+        public string Search { get; set; }
+    }
+
+	[RoutePrefix("api/Iis")]
+    public class IisController : ApiController
+    {
+        private Result Execute(Action<Action<string>> action)
+        {
+            var stb = new StringBuilder();
+            Action<string> act = (x) => stb.AppendLine(x);
+            try
+            {
+                action(act);
+                return new Result { Success = true, Message = stb.ToString() };
+            }
+            catch (Exception ex)
+            {
+                act(ex.Message);
+                act(ex.StackTrace);
+                return new Result { Success = false, Message = stb.ToString() };
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateSite")]
+        public Result CreateSite([FromBody] CreateSiteRequest request)
+        {
+            return Execute((act) => IisManager.CreateSite(act, request.PoolName, request.SiteName, request.SiteUrl, request.SiteFolder));
+        }
+
+        [HttpPost]
+        [Route("CreateApplication")]
+        public Result CreateApplication([FromBody] CreateApplicationRequest request)
+        {
+            return Execute((act) => IisManager.CreateApplication(act, request.PoolName, request.SiteName, request.AppPath, request.PhisicalPath));
+        }
+
+        [HttpPost]
+        [Route("DeleteSiteWithPull")]
+        public Result DeleteSiteWithPull([FromBody] DeleteSiteWithPullRequest request)
+        {
+            return Execute((act) => IisManager.DeleteSiteWithPull(act, request.SiteName));
+        }
+
+        [HttpPost]
+        [Route("CreatePool")]
+        public Result CreatePool([FromBody] PoolRequest request)
+        {
+            return Execute((act) => IisManager.CreatePool(act, request.PoolName));
+        }
+
+        [HttpPost]
+        [Route("StartPool")]
+        public Result StartPool([FromBody] PoolRequest request)
+        {
+            return Execute((act) => IisManager.StartPool(act, request.PoolName));
+        }
+
+        [HttpPost]
+        [Route("StopPool")]
+        public Result StopPool([FromBody] PoolRequest request)
+        {
+            return Execute((act) => IisManager.StopPool(act, request.PoolName));
+        }
+
+        [HttpPost]
+        [Route("DeletePull")]
+        public Result DeletePull([FromBody] PoolRequest request)
+        {
+            return Execute((act) => IisManager.DeletePull(act, request.PoolName));
+        }
+
+        [HttpPost]
+        [Route("GetPoolWithoutSites")]
+        public Result GetPoolWithoutSites([FromBody] GetPoolWithoutSitesRequest request)
+        {
+            List<string> test = null;
+            var r = Execute((act) => test = IisManager.GetPoolWithoutSites(act));
+            r.Additional = test;
+            return r;
+        }
+
+        [HttpPost]
+        [Route("GetSiteNames")]
+        public Result GetSiteNames([FromBody] GetSiteNamesRequest request)
+        {
+            List<string> test = null;
+            var r = Execute((act) => test = IisManager.GetSiteNames(act, request.Search));
+            r.Additional = test;
+            return r;
+        }
+    }
+}
---
+++ IisHelperService/Domain/IisManager.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IisHelperService.Domain
+{
+    public static class IisManager
+    {
+        public static void CreateSite(Action<string> consoleWriteLine, string poolName, string siteName, string siteUrl, string siteFolder)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                if (serverManager.ApplicationPools.Any(x => x.Name == poolName))
+                {
+                    consoleWriteLine("PULL EXISTS: " + poolName);
+                }
+                else
+                {
+                    ApplicationPool newPool = serverManager.ApplicationPools.Add(poolName);
+                    newPool.Enable32BitAppOnWin64 = false;
+                    newPool.ManagedRuntimeVersion = "v4.0";
+                    consoleWriteLine("TRY CREATE PULL: " + poolName);
+                }
+                if (serverManager.Sites.Any(x => x.Name == siteName))
+                {
+                    consoleWriteLine("SITE EXISTS: " + siteName);
+                }
+                else
+                {
+                    var site = serverManager.Sites.Add(siteName, "http", siteUrl, siteFolder);
+                    site.ApplicationDefaults.ApplicationPoolName = poolName;
+                    site.ServerAutoStart = true;
+                    consoleWriteLine("TRY CREATE SITE: " + siteName);
+                }
+                serverManager.CommitChanges();
+                consoleWriteLine("SUCCESS!");
+            }
+        }
+
+        public static void CreateApplication(Action<string> consoleWriteLine, string poolName, string siteName, string appPath, string phisicalPath)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                if (serverManager.ApplicationPools.Any(x => x.Name == poolName))
+                {
+                    consoleWriteLine("PULL EXISTS: " + poolName);
+                }
+                else
+                {
+                    ApplicationPool newPool = serverManager.ApplicationPools.Add(poolName);
+                    newPool.Enable32BitAppOnWin64 = false;
+                    // todo  ManagedRuntimeVersion вынести везде в параметр над
+                    newPool.ManagedRuntimeVersion = "";
+                    consoleWriteLine("TRY CREATE PULL: " + poolName);
+                }
+
+                var site = serverManager.Sites.FirstOrDefault(x => x.Name == siteName);
+                if (site != null)
+                {
+                    site.Applications.Add(appPath, phisicalPath);
+                    consoleWriteLine("TRY CREATE APP: " + appPath);
+                }
+                else
+                {
+                    consoleWriteLine("SITE NOT EXISTS: " + siteName);
+                }
+                serverManager.CommitChanges();
+                consoleWriteLine("SUCCESS!");
+            }
+        }
+
+        public static void CreatePool(Action<string> consoleWriteLine, string poolName)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var pool = serverManager.ApplicationPools.FirstOrDefault(x => x.Name == poolName);
+                if (pool != null)
+                {
+                    consoleWriteLine("PULL EXISTS: " + poolName);
+                }
+                else
+                {
+                    ApplicationPool newPool = serverManager.ApplicationPools.Add(poolName);
+                    newPool.Enable32BitAppOnWin64 = false;
+                    newPool.ManagedRuntimeVersion = "v4.0";
+                    consoleWriteLine("TRY CREATE PULL: " + poolName);
+                    serverManager.CommitChanges();
+                    consoleWriteLine("SUCCESS!");
+                }
+            }
+        }
+
+        public static void StartPool(Action<string> consoleWriteLine, string poolName)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var pool = serverManager.ApplicationPools.FirstOrDefault(x => x.Name == poolName);
+                if (pool != null)
+                {
+                    if (pool.State != ObjectState.Stopped)
+                    {
+                        consoleWriteLine("PULL BAD STATE: " + pool.State);
+                    }
+                    else
+                    {
+                        consoleWriteLine("TRY PULL START " + poolName);
+                        pool.Start();
+                        consoleWriteLine("SUCCESS!");
+                    }
+                }
+                else
+                {
+                    consoleWriteLine("PULL NOT EXISTS: " + poolName);
+                }
+            }
+        }
+
+        public static void StopPool(Action<string> consoleWriteLine, string poolName)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var pool = serverManager.ApplicationPools.FirstOrDefault(x => x.Name == poolName);
+                if (pool != null)
+                {
+                    if (pool.State != ObjectState.Started)
+                    {
+                        consoleWriteLine("PULL BAD STATE: " + pool.State);
+                    }
+                    else
+                    {
+                        consoleWriteLine("TRY PULL STOP " + poolName);
+                        pool.Stop();
+                        consoleWriteLine("SUCCESS!");
+                    }
+                }
+                else
+                {
+                    consoleWriteLine("PULL NOT EXISTS: " + poolName);
+                }
+            }
+        }
+
+        public static List<string> GetPoolWithoutSites(Action<string> consoleWriteLine)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var poolNames = serverManager.ApplicationPools.Select(x => x.Name).ToList();
+                var sitesPoolsNames = serverManager.Sites.Select(x => x.Applications.First().ApplicationPoolName).ToList();
+                poolNames = poolNames.Where(x => !sitesPoolsNames.Contains(x)).ToList();
+                return poolNames;
+            }
+        }
+
+        public static List<string> GetSiteNames(Action<string> consoleWriteLine, string search)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var poolNames = serverManager.Sites.Select(x => x.Name).ToList();
+                if (!String.IsNullOrEmpty(search))
+                {
+                    poolNames = poolNames.Where(x => x.ToLower().Contains(search)).ToList();
+                }
+                return poolNames;
+            }
+        }
+
+        public static void DeleteSiteWithPull(Action<string> consoleWriteLine, string siteName)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var site = serverManager.Sites.FirstOrDefault(x => x.Name == siteName);
+                if (site == null)
+                {
+                    consoleWriteLine("SITE NOT EXISTS: " + siteName);
+                }
+                else
+                {
+                    var poolName = site.ApplicationDefaults.ApplicationPoolName;
+                    consoleWriteLine("TRY SITE DELETE" + siteName);
+                    serverManager.Sites.Remove(site);
+                    var pool = serverManager.ApplicationPools.FirstOrDefault(x => x.Name == poolName);
+                    if (pool == null)
+                    {
+                        consoleWriteLine("PULL NOT EXISTS: " + poolName);
+                    }
+                    else
+                    {
+                        consoleWriteLine("TRY PULL DELETE " + poolName);
+                        serverManager.ApplicationPools.Remove(pool);
+                    }
+                    serverManager.CommitChanges();
+                    consoleWriteLine("SUCCESS!");
+                }
+            }
+        }
+
+        public static void DeletePull(Action<string> consoleWriteLine, string poolName)
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var pool = serverManager.ApplicationPools.FirstOrDefault(x => x.Name == poolName);
+                if (pool == null)
+                {
+                    consoleWriteLine("PULL NOT EXISTS: " + poolName);
+                }
+                else
+                {
+                    consoleWriteLine("TRY PULL DELETE " + poolName);
+                    serverManager.ApplicationPools.Remove(pool);
+                }
+                serverManager.CommitChanges();
+                consoleWriteLine("SUCCESS!");
+            }
+        }
+    }
+}
